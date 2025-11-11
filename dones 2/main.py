916ec927cd43184468af209a1fcf07a742a1d8e9
lautora05 <lautarot078@gmail.com>
2025-11-10 21:09:26 -0300

    Delete dones 2 directory

--- dones 2/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import (QApplication)
-from main_window10 import VentanaMonitoreoDrones
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ventana = VentanaMonitoreoDrones()
-    ventana.show()
-    sys.exit(app.exec_())
